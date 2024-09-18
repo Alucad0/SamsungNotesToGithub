9c10f52cb6beba495ba2b712fca86c91409ad831
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2024-09-18 21:05:35 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import json
+import requests
+from github3 import login
+from pygalaxy import GalaxyClient
+
+def export_samsung_notes(note_files: [], repo_name: str):
+  pass
+
+
+if __name__ == "__main__":
+  # export_samsung_notes()
